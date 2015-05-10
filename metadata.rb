5ad9f81b13b380f3f728acffe91b75931349df0e
Damacus <dan.m.webb@gmail.com>
2015-05-10 10:52:12 +0100

    update version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ai@derby.ac.uk'
 license 'Apache 2.0'
 description 'Installs & configures tuxedo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+version '0.1.0' 
 
 depends 'resource'
 depends 'ark'
